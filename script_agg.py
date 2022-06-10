a90c2e79eaa8e4cc995676eb992038e3f5818412
Ryoma Chidiwa <rrr.h.o.f@gmail.com>
2022-06-11 02:48:04 +0900

    add file.

--- script_agg.py
+++ script_agg.py
@@ -2,10 +2,22 @@ import pyautogui as pg
 import time
 import openpyxl
 import datetime
-# 対象パス C:\Users\竜馬\OneDrive\share\集計転記\syuukeiin.xlsx
+# excelbookパス C:\Users\竜馬\OneDrive\share\集計転記\syuukeiin.xlsx
 
 dt_now = datetime.datetime.now()
+print(dt_now.strftime('%Y%m%d')) 
 
-print(dt_now) # 2022-06-09 01:03:52.865150(出力時)
+wb = openpyxl.load_workbook(r'C:\Users\竜馬\OneDrive\share\集計転記\syuukeiin.xlsx')
+ws = wb.worksheets[0]
+maxClm = ws.max_column
+maxRow = ws.max_row
+print('maxcolumn : ' + str(maxClm))
+print('maxrow    : ' + str(maxRow))
 
-print(dt_now.strftime('%Y%m%d')) # 20220609
+for i in range(2, maxClm + 1, 1):
+    for j in reversed(range(1,maxRow + 1)):
+        if ws.cell(j, i).value != None:
+            print(str(i) + '列の最終行 : ' + str(j))
+            break
+
+
