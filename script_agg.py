01d36f8f53dfad7b7cae145e6b7636d31be569f8
Ryoma Chidiwa <rrr.h.o.f@gmail.com>
2022-06-14 01:48:58 +0900

    add file.

--- script_agg.py
+++ script_agg.py
@@ -4,17 +4,26 @@ import openpyxl
 import datetime
 import subprocess
 import win32gui
+import win32con
+import win32api
 from tkinter import messagebox
 
 subprocess.Popen(r'C:\Windows\notepad.exe')
-memoapp = win32gui.FindWindow(None, '無題 - メモ帳')
-time.sleep(1)
-win32gui.SetForegroundWindow(memoapp) # エラーあり
+time.sleep(2)
+def foreground():
+    hwnd = win32gui.FindWindow(None, '無題 - メモ帳')
+    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    pg.moveTo(left + 60, top + 10)
+    pg.click()
+    win32gui.SetWindowPos(hwnd, win32con.HWND_NOTOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+if __name__ == '__main__':
+    foreground()
 
 dt_now = datetime.datetime.now()
 print(dt_now.strftime('%Y%m%d'))
 
-# excelファイルよりデータ取得
 wb = openpyxl.load_workbook(r'C:\Users\竜馬\OneDrive\share\集計転記\syuukeiin.xlsx')
 ws = wb.worksheets[0]
 maxClm = ws.max_column
@@ -28,7 +37,7 @@ for j in range(2, maxClm + 1):
                 values = []
                 for clm in row:
                     values.append(clm.value)
-# 基本情報の入力処理
+
             time.sleep(1)
             pg.write(str(values[0]))
             pg.press('enter')
@@ -36,7 +45,7 @@ for j in range(2, maxClm + 1):
             pg.press('enter')
             pg.write(dt_now.strftime('%Y%m%d'))
             pg.press('enter')
-# オーダー数の処理
+
             for k in range(1, i):
                 pg.write(str(values[k]))
                 pg.press('enter')
@@ -45,7 +54,8 @@ for j in range(2, maxClm + 1):
             time.sleep(2)
             print(str(j) + '列の最終行 : ' + str(i))
             print(values)
-
             break
+
 wb.close()
+
 messagebox.showinfo('自動入力', '処理が完了しました。')
