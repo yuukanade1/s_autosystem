ff4d3da00e911c1ea166d4e3ad7fb0cd5958d951
Ryoma Chidiwa <rrr.h.o.f@gmail.com>
2022-06-15 00:29:53 +0900

    add file.

--- script_agg.py
+++ script_agg.py
@@ -6,6 +6,7 @@ import subprocess
 import win32gui
 import win32con
 import win32api
+import pyperclip
 from tkinter import messagebox
 
 subprocess.Popen(r'C:\Windows\notepad.exe')
@@ -22,14 +23,14 @@ if __name__ == '__main__':
     foreground()
 
 dt_now = datetime.datetime.now()
-print(dt_now.strftime('%Y%m%d'))
+# print(dt_now.strftime('%Y%m%d'))
 
 wb = openpyxl.load_workbook(r'C:\Users\竜馬\OneDrive\share\集計転記\syuukeiin.xlsx')
 ws = wb.worksheets[0]
 maxClm = ws.max_column
 maxRow = ws.max_row
-print('maxcolumn : ' + str(maxClm))
-print('maxrow    : ' + str(maxRow))
+# print('maxcolumn : ' + str(maxClm))
+# print('maxrow    : ' + str(maxRow))
 for j in range(2, maxClm + 1):
     for i in reversed(range(1,maxRow + 1)):
         if ws.cell(i, j).value != None:
@@ -45,17 +46,19 @@ for j in range(2, maxClm + 1):
             pg.press('enter')
             pg.write(dt_now.strftime('%Y%m%d'))
             pg.press('enter')
-
-            for k in range(1, i):
+            pyperclip.copy(values[1])
+            pg.hotkey('ctrl', 'v')
+            pg.press('enter')
+            for k in range(2, i):
                 pg.write(str(values[k]))
                 pg.press('enter')
+            # pg.press('F7')
             pg.write('F7')
             pg.press('enter')
             time.sleep(2)
-            print(str(j) + '列の最終行 : ' + str(i))
-            print(values)
+            # print(str(j) + '列の最終行 : ' + str(i))
+            # print(values)
             break
 
 wb.close()
-
 messagebox.showinfo('自動入力', '処理が完了しました。')
